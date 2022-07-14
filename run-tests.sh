b336f77390c2e7a9de66f6e155280ae5b77a6a7e
Mike Devereux <Michael.Devereux@unibas.ch>
2022-07-14 15:16:26 +0200

    Mike Devereux: added fMDCM test case

--- run-tests.sh
+++ run-tests.sh
@@ -3,14 +3,14 @@
 # This script calls each of the DCM tests in the range STRT to STOP
 
 # Set some variables to be used by the test scripts
-export DEVCHARMM=/opt/cluster/programs/charmm/developer/dev-release-dcm/build/cmake/charmm # development version to be tested
-export REFCHARMM=/home/devereux/c45a1/build/cmake/charmm # reference stable CHARMM release
+export DEVCHARMM=/opt/cluster/programs/charmm/developer/dev-release-fmdcm/bin/charmm # development version to be tested
+export REFCHARMM=/opt/cluster/programs/charmm/developer/dev-release-dcm/build/cmake/charmm # reference stable CHARMM release
 export NPROC=4 # number of MPI cores to use for the tests
 export WORKDIR=$(pwd) # Set the root folder containing the tests
 
 # Set some local parameters
 STRT=1  # First test to run (1)
-STOP=16 # Last test to run (16)
+STOP=17 # Last test to run (17)
 
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
